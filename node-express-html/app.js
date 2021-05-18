b1311ed0e2e182337a42fd0c5cdac66f290d349c
Stanislav Chvila <schila@gmail.com>
2021-05-18 11:54:52 +0200

    express html academind version

--- node-express-html/app.js
+++ node-express-html/app.js
@@ -1,17 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser')
 const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop')
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(adminRoutes);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views', 'page-notfound.html'));
-})
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
 app.listen(5000);
--- node-express-html/routes/admin.js
+++ node-express-html/routes/admin.js
@@ -1,15 +1,20 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
-router.get('/add-product',(req, res, next)=>{
-    res.sendFile(path.join(__dirname,'../', 'views', 'add-product.html'));
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
-// only for POST requests no GET
-router.post('/product',(req, res, next)=>{
-    console.log(req.body);
-    res.send('<h1>Product2</h1>')
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 module.exports = router;
