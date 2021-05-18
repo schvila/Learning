a17ef18276a21ff80d941b429c8173763576552c
Stanislav Chvila <schila@gmail.com>
2021-05-18 15:23:54 +0200

    typescript workspace change

--- TypeScript/BasicTypes/app.ts
+++ TypeScript/BasicTypes/app.ts
@@ -1,1 +1,7 @@
-console.log('test log');
+function add(n1: number, n2: number) {
+  console.log(typeof n1);
+  return n1 + n2;
+}
+const number1 = 5;
+const number2 = 2.8;
+console.log(add(number1, number2));
