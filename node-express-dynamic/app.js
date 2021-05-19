1533b6bad4b50eb3aa2662d01280cf111c9b2a7a
Stanislav Chvila <stanislav.chvila@thermofisher.com>
2021-05-19 09:22:59 +0200

    express pug template added

--- node-express-dynamic/app.js
+++ node-express-dynamic/app.js
@@ -5,17 +5,20 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes.routes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.listen(5000);
+app.listen(3000);
--- node-express-dynamic/routes/admin.js
+++ node-express-dynamic/routes/admin.js
@@ -5,7 +5,7 @@ const express = require('express');
 const rootDir = require('../util/path');
 
 const router = express.Router();
-//just test 
+
 const products = [];
 
 // /admin/add-product => GET
@@ -15,8 +15,7 @@ router.get('/add-product', (req, res, next) => {
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  //console.log(req.body);
-  products.push({title: req.body.title});
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
