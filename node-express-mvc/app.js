3afc38ed5e444d9c16058e9a96f1c55f369ef72c
Stanislav Chvila <stanislav.chvila@thermofisher.com>
2021-05-20 04:50:35 +0200

    bodyParser removed

--- node-express-mvc/app.js
+++ node-express-mvc/app.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const express = require('express');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -12,7 +12,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error')
 
-app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes.routes);
