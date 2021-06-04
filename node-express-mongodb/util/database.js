6acb61cb59310bc058494e0701cd2231134bb0b7
Stanislav Chvila <stanislav.chvila@thermofisher.com>
2021-06-04 12:23:51 +0200

    my mongo standa connection

--- node-express-mongodb/util/database.js
+++ node-express-mongodb/util/database.js
@@ -2,10 +2,10 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 let _db;
-
+// 'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+    'mongodb+srv://standa:rnEZ3rjw3lf9J7Bh@cluster0.5tfx5.mongodb.net/myFirstDatabase?retryWrites=true'
   )
     .then(client => {
       console.log('Connected!');
