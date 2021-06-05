73c465d0403915d857d72aed4988983c7c5c7598
Stanislav Chvila <stanislav.chvila@thermofisher.com>
2021-06-05 08:22:27 +0200

    fixing mogodb

--- node-express-mongodb/util/database.js
+++ node-express-mongodb/util/database.js
@@ -5,7 +5,7 @@ let _db;
 // 'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://standa:rnEZ3rjw3lf9J7Bh@cluster0.5tfx5.mongodb.net/myFirstDatabase?retryWrites=true'
+    'mongodb+srv://standa:rnEZ3rjw3lf9J7Bh@cluster0.5tfx5.mongodb.net/shop?retryWrites=true'
   )
     .then(client => {
       console.log('Connected!');
