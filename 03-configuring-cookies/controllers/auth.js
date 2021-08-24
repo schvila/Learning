d16de6b4316d832d65b9bc9fc2f6c875488a865e
Stanislav Chvila <schvila@gmail.com>
2021-08-24 06:58:24 +0200

    fix Cookie.

--- 03-configuring-cookies/controllers/auth.js
+++ 03-configuring-cookies/controllers/auth.js
@@ -1,17 +1,18 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req
-    .get('Cookie')
-    .split(';')[1]
-    .trim()
-    .split('=')[1];
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login',
-    isAuthenticated: isLoggedIn
+  console.log("check cookie:");
+  console.log(req.get("Cookie"));
+  let isLoggedIn = false;
+  if (req.get("Cookie")) {
+    isLoggedIn = req.get("Cookie").trim().split("=")[1];
+  }
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: isLoggedIn,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true');
-  res.redirect('/');
+  res.setHeader("Set-Cookie", "loggedIn=true");
+  res.redirect("/");
 };
